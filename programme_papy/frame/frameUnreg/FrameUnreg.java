ba43361cc671195ff6c8ed9a62b2ceb965cb6002
titouan <bahon.titouan@gmail.com>
2021-05-16 15:47:58 +0200

    affichage UnReg - Titouan

--- programme_papy/frame/frameUnreg/FrameUnreg.java
+++ programme_papy/frame/frameUnreg/FrameUnreg.java
@@ -6,6 +6,7 @@ import java.awt.*;
 public class FrameUnreg extends JFrame {
     public static JFrame frameUnreg = new JFrame();
     Color CouleurBleu = new Color(83, 122, 204);
+    Color CouleurBleuClaire = new Color(133, 156, 227);
     public FrameUnreg(){
         frameUnreg.setTitle("Programme de marche");
         frameUnreg.setSize(1200, 700);
@@ -16,16 +17,24 @@ public class FrameUnreg extends JFrame {
         frameUnreg.getContentPane().setBackground(CouleurBleu);
         frameUnreg.setLayout(new BorderLayout());
 
-        //Panel du haut
-        PanelCentreUnreg panelDuHaut = new PanelCentreUnreg();
-        frameUnreg.add(panelDuHaut.fonctionPanelHaut(), BorderLayout.CENTER);
-        ///////////////
+
+        //Panel gauche
+        JPanel gauche = new JPanel();
+        gauche.setBackground(CouleurBleuClaire);
+        gauche.setPreferredSize(new Dimension(frameUnreg.getWidth()/3,1));
+        frameUnreg.add(gauche,BorderLayout.WEST);
+
+        //Panel droite
+        JPanel droite = new JPanel();
+        droite.setBackground(CouleurBleuClaire);
+        droite.setPreferredSize(new Dimension(frameUnreg.getWidth()/3,1));
+        frameUnreg.add(droite,BorderLayout.EAST);
 
         //Panel du centre
-        PanelGaucheUnreg panelGaucheUnreg = new PanelGaucheUnreg(this);
-        JPanel tmp = panelGaucheUnreg.fonctionPanelGaucheUnreg();
+        PanelCentreUnreg panelCentreUnreg = new PanelCentreUnreg(this);
+        JPanel tmp = panelCentreUnreg.fonctionPanelCentreUnreg();
         tmp.setPreferredSize(new Dimension(300, 200));
-        frameUnreg.add(tmp, BorderLayout.WEST);
+        frameUnreg.add(tmp, BorderLayout.CENTER);
         //////////////
         frameUnreg.setVisible(true);
     }
--- programme_papy/frame/frameUnreg/PanelCentreUnreg.java
+++ programme_papy/frame/frameUnreg/PanelCentreUnreg.java
@@ -1,42 +1,198 @@
 package frame.frameUnreg;
 
+import frame.TextPrompt;
+import frame.frameReg.FrameReg;
 import javax.swing.*;
+import javax.swing.border.MatteBorder;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
 
-public class PanelCentreUnreg {
+import static frame.frameUnreg.FrameUnreg.frameUnreg;
+
+public class PanelCentreUnreg implements MouseListener {
+    JTextField textFieldNom = new JTextField(20);
+    JTextField textFieldPrenom = new JTextField(20);
+    JTextField textFieldAge = new JTextField(5);
+    JTextField textFieldPoids = new JTextField(5);
     JPanel panelCentre = new JPanel();
-    JLabel labelHaut = new JLabel();
-    Color couleurRose = new Color(169, 129, 155);
-    public PanelCentreUnreg() {
-        panelCentre.setBackground(couleurRose);
-        panelCentre.setLayout(new BorderLayout());
+    FrameUnreg frame;
+    Color CouleurBleu = new Color(83, 122, 204);
+    Color CouleurBleuClair = new Color(133, 156, 227);
+    MatteBorder bordureTextfield = new MatteBorder(0,0,2,0, Color.white);
+    Font police = new Font("Arial", Font.BOLD, 14);
+    JLabel enregistrer = new JLabel("Enregistrer");
+    JPanel panelEnregistrer = new JPanel(new FlowLayout());
 
-        //Panel du haut
-        JPanel panelCentreHaut = new JPanel(new BorderLayout());
-        panelCentreHaut.setBackground(couleurRose);
-        panelCentreHaut.setPreferredSize(new Dimension(100, 150));
-        JPanel panelCentreHautBas = new JPanel(new FlowLayout());
-        panelCentreHautBas.setBackground(couleurRose);
-        labelHaut.setText("Bienvenue dans ce programme de marche !");
-        labelHaut.setFont(new Font("TimesRoman", Font.PLAIN, 20));
-        panelCentreHautBas.add(labelHaut);
-        panelCentreHaut.add(panelCentreHautBas, BorderLayout.SOUTH);
-        panelCentre.add(panelCentreHaut, BorderLayout.NORTH);
-        //////////////////////////////
+    JPanel panelGauche = new JPanel(new BorderLayout());
+    JPanel panelHaut = new JPanel();
 
-        //Panel du centre
-        JLabel labelCentre = new JLabel("trouver un truc à mettre");
-        labelCentre.setFont(new Font("TimesRoman", Font.PLAIN, 14));
-        JPanel panelCentreCentre = new JPanel(new GridBagLayout());
-        panelCentreCentre.setBackground(couleurRose);
-        GridBagConstraints contraintes = new GridBagConstraints();
-        contraintes.gridx = 0;
-        contraintes.gridy = 0;
-        panelCentreCentre.add(labelCentre, contraintes);
-        panelCentre.add(panelCentreCentre, BorderLayout.CENTER);
-        //////////////////////////////
+
+    public PanelCentreUnreg(FrameUnreg fr){
+        this.frame = fr;
+        //=====Panel Centre============================
+        panelCentre.setLayout(new GridBagLayout());
+        GridBagConstraints constraint = new GridBagConstraints();
+        constraint.insets = new Insets(10, 10, 10, 10);
+
+        constraint.gridx = 0;
+        constraint.gridy = 0;
+        textFieldNom.setOpaque(false);
+        textFieldNom.setBorder(bordureTextfield);
+        textFieldNom.setForeground(Color.white);
+        textFieldNom.setFont(police);
+        TextPrompt TpNom = new TextPrompt("Nom", textFieldNom); //Le textPrompt c'est pour ne mettre le texte que quand le jtextfield n'as pas le focus ou est vide
+        TpNom.setFont(police);
+        TpNom.setShow(TextPrompt.Show.ALWAYS);
+        panelCentre.add(textFieldNom, constraint);
+
+        constraint.gridx = 0;
+        constraint.gridy = 1;
+        textFieldPrenom.setOpaque(false);
+        textFieldPrenom.setBorder(bordureTextfield);
+        textFieldPrenom.setForeground(Color.white);
+        textFieldPrenom.setFont(police);
+        TextPrompt TpPrenom = new TextPrompt("Prénom", textFieldPrenom);
+        TpPrenom.setFont(police);
+        TpPrenom.setShow(TextPrompt.Show.ALWAYS);
+        panelCentre.add(textFieldPrenom, constraint);
+
+        constraint.gridx = 0;
+        constraint.gridy = 2;
+        textFieldAge.setOpaque(false);
+        textFieldAge.setBorder(bordureTextfield);
+        textFieldAge.setForeground(Color.white);
+        textFieldAge.setFont(police);
+        textFieldAge.setHorizontalAlignment(JTextField.CENTER);
+        TextPrompt TpAge = new TextPrompt("Age", textFieldAge);
+        TpAge.setFont(police);
+        TpAge.setHorizontalAlignment(JTextField.CENTER);
+        TpAge.setShow(TextPrompt.Show.ALWAYS);
+        panelCentre.add(textFieldAge, constraint);
+
+        constraint.gridx = 0;
+        constraint.gridy = 4;
+        textFieldPoids.setOpaque(false);
+        textFieldPoids.setBorder(bordureTextfield);
+        textFieldPoids.setForeground(Color.white);
+        textFieldPoids.setFont(police);
+        textFieldPoids.setHorizontalAlignment(0);
+        TextPrompt TpPoids = new TextPrompt("Poids", textFieldPoids);
+        TpPoids.setFont(police);
+        TpPoids.setHorizontalAlignment(0);
+        TpPoids.setShow(TextPrompt.Show.ALWAYS);
+        panelCentre.add(textFieldPoids, constraint);
+
+        constraint.gridx = 0;
+        constraint.gridy = 5;
+
+        enregistrer.setForeground(Color.white);
+        enregistrer.setFont(new Font("Arial", Font.BOLD, 16));
+        panelEnregistrer.setBackground(CouleurBleu);
+        panelEnregistrer.add(enregistrer);
+        panelEnregistrer.addMouseListener(this);
+        panelCentre.add(panelEnregistrer, constraint);
+
+        panelCentre.setBackground(CouleurBleu);
+        panelGauche.add(panelCentre, BorderLayout.CENTER);
+        //========================================================
+        //=======Panel du Haut======================
+        panelHaut.setLayout(new BorderLayout());
+        panelHaut.setPreferredSize(new Dimension(300, 250));
+        panelHaut.setBackground(CouleurBleu);
+
+        //J'ajoute l'image tout en haut du panel pour s'enregistrer
+        JLabel LabelImage = new JLabel();
+        Image iconePersonne = new ImageIcon("programme_papy/image/iconePersonne.png").getImage();
+        ImageIcon img2 = new ImageIcon(new ImageIcon(iconePersonne).getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH));
+        LabelImage.setIcon(img2);
+        JPanel panelHautCentre = new JPanel(new FlowLayout());
+        panelHautCentre.setBackground(CouleurBleu);
+        panelHautCentre.add(LabelImage);
+        panelHaut.add(panelHautCentre, BorderLayout.CENTER);
+        //////////////////////////////////////////////////////////
+
+        //J'ajoute le label "enregistrement
+        JLabel labelEnregistrer = new JLabel("Enregistrement");
+        labelEnregistrer.setFont(new Font("Arial", Font.ITALIC, 24));
+        labelEnregistrer.setForeground(Color.white);
+        JPanel panelHautBas = new JPanel(new FlowLayout());
+        panelHautBas.setBackground(CouleurBleu);
+        panelHautBas.add(labelEnregistrer);
+        panelHaut.add(panelHautBas, BorderLayout.SOUTH);
+        //////////////////////////////////////////////////////////
+
+        //J'ajoute un peu d'espace en haut
+        JPanel panelHautHaut = new JPanel(new FlowLayout());
+        panelHautHaut.setPreferredSize(new Dimension(100, 50));
+        panelHautHaut.setBackground(CouleurBleu);
+        panelHaut.add(panelHautHaut, BorderLayout.NORTH);
+        //////////////////////////////////////
+        panelGauche.add(panelHaut, BorderLayout.NORTH);
+        //=========================================================
+
+        //========Panel du bas======================
+        JPanel panelSud = new JPanel(new FlowLayout());
+        panelSud.setBackground(CouleurBleu);
+        panelSud.setPreferredSize(new Dimension(100, 100));
+        panelGauche.add(panelSud, BorderLayout.SOUTH);
+        //==========================================
     }
-    public JPanel fonctionPanelHaut(){
-        return this.panelCentre;
+
+    public JPanel fonctionPanelCentreUnreg(){
+        return panelGauche;
+    }
+
+
+    public void ecrireFichier(frame.Personne p1) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("programme_papy/donnee/utilisateur.txt"));//On choisi le fichier dans lequel on écrit
+            bw.write(p1.toString());//On écrit
+            bw.close();
+            LocalDate date = null;
+            BufferedWriter bw2 = new BufferedWriter(new FileWriter("programme_papy/donnee/ancienPoids.txt"));//On choisi le fichier dans lequel on écrit
+            bw2.write(p1.getPoids() +"," + date.now() + "\n");//On écrit
+            bw2.close();
+        }
+        catch (IOException e)//Si il y a une erreur on la récupère.
+        {
+            System.out.print(e.getMessage());
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        String userNom = textFieldNom.getText();
+        String userPrenom = textFieldPrenom.getText();
+        int userAge = Integer.parseInt(textFieldAge.getText());
+        int userPoids = Integer.parseInt(textFieldPoids.getText());
+        frame.Personne p1 = new frame.Personne(userNom, userPrenom, userAge, userPoids);
+        ecrireFichier(p1);
+        frameUnreg.dispose();
+        FrameReg frameReg = new FrameReg();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        panelEnregistrer.setBackground(CouleurBleuClair);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        panelEnregistrer.setBackground(CouleurBleu);
     }
 }
